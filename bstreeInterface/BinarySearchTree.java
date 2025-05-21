91d9e9f2c9372df5c94494b0c371d623d5efb642
GiusseppeInfa <142847355+GiusseppeInfa@users.noreply.github.com>
2025-05-20 23:49:27 -0500

    Implementación de nuevos métodos a la interfaz, para destruir todos los nodos, contar todos los nodos no hojas, calcular la altura del arbol y su amplitud(cantidad de nodos maximos en un nivel)

--- bstreeInterface/BinarySearchTree.java
+++ bstreeInterface/BinarySearchTree.java
@@ -7,6 +7,12 @@ import Exceptions.ItemNotFound;
 public interface BinarySearchTree<E> {
 	void insert(E data) throws ItemDuplicated;
 	E search(E data) throws ItemNotFound;
-	void delete(E data) throws ExceptionIsEmpty;
+	void delete(E data) throws ExceptionIsEmpty; //, ItemNotFound
 	boolean isEmpty();
+	
+	void destroyNodes() throws ExceptionIsEmpty;
+	int countAllNodes();
+	int countNodes();
+	int height(E x);
+	int amplitude(int level);
 }
